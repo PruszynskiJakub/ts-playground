c1b7fb37ffa7ba32416487f2c75683abb796b12f
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2025-05-29 17:49:35 +0200

    commit

--- qdrant/app.ts
+++ qdrant/app.ts
@@ -1,14 +1,3 @@
-import { env } from 'bun';
-import { QdrantClient } from "@qdrant/js-client-rest";
+import {getCollections} from "./vectore.store.ts";
 
-const client = new QdrantClient({
-    url: env.QDRANT_URL,
-    apiKey: env.QDRANT_API_KEY
-});
-
-try {
-    const result = await client.getCollections();
-    console.log('List of collections:', result.collections);
-} catch (err) {
-    console.error('Could not get collections:', err);
-}
+await getCollections()
---
+++ qdrant/vectore.store.ts
@@ -0,0 +1,16 @@
+import { env } from 'bun';
+import { QdrantClient } from "@qdrant/js-client-rest";
+
+const client = new QdrantClient({
+    url: env.QDRANT_URL,
+    apiKey: env.QDRANT_API_KEY
+});
+
+export async function getCollections() {
+    try {
+        const result = await client.getCollections();
+        console.log('List of collections:', result.collections);
+    } catch (err) {
+        console.error('Could not get collections:', err);
+    }
+}
