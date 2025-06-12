518f4ebf22ae23720355a79e62f91c864a13ac34
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2025-06-12 12:45:55 +0200

    feat: add basic LangChain translation app with OpenAI GPT-4o

---
+++ langchain/app.ts
@@ -0,0 +1,11 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+
+const model = new ChatOpenAI({ model: "gpt-4o" });
+const messages = [
+    new SystemMessage("Translate the following from English into Italian"),
+    new HumanMessage("hi!"),
+];
+
+let message = await model.invoke(messages);
+console.log(message)
