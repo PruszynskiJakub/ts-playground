fc74b05c738a59b1393d7426ba319a487316517c
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2025-07-15 13:37:18 +0200

    Create Document interface

--- document/types.ts
+++ document/types.ts
@@ -1,10 +1,19 @@
 export interface Document {
-  uuid: string;
   content: string;
   metadata: {
-    tokens: number,
-    source?: string;
+    tokens: number;
+    source?: string; // url / path
+    mimeType?: string; // mime type
+    name?: string; // filename
+    source_uuid?: string;
+    conversation_uuid?: string;
+    uuid?: string;
+    duration?: number; // duration in seconds
+    headers?: Headers;
     urls?: string[];
     images?: string[];
+    screenshots?: string[];
+    chunk_index?: number;
+    total_chunks?: number;
   };
 }
